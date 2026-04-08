36977de8f06e74259b57611dd4e9ca15b2c13fd0
DAE JOO <j00112459@gmail.com>
2026-04-08 15:37:10 +0900

    feat: 캘린더 구현
    * 달력, 내역수정
    
    * fix; pages 폴더로 이동
    
    * 달력, 내역수정
    
    * fix; pages 폴더로 이동
    
    * 달력, 내역수정
    
    * fix; pages 폴더로 이동
    
    ---------
    
    Co-authored-by: j00112459 <j0011459@gmail.com>
    Co-authored-by: SongTaeKwon <taekwon0321@gmail.com>
    Co-authored-by: Song TaeKwon <79988462+SongTaeKwon@users.noreply.github.com>

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import { useUserStore } from '@/stores/user.js';
+
+import MonthlyHistory from '@/pages/MonthlyHistory.vue';
+import EditRecord from '@/pages/EditRecord.vue';
 import MainPage from '@/pages/main/MainPage.vue';
 import ChartPage from '@/pages/chart/ChartPage.vue';
 import AddPage from '@/pages/add/AddPage.vue';
@@ -8,6 +11,8 @@ import ShareListPage from '@/pages/share/ShareListPage.vue';
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    { path: '/monthly', component: MonthlyHistory },
+    { path: '/edit', component: EditRecord },
     { path: '/share', name: 'share', component: ShareListPage },
     { path: '/', component: MainPage },
     { path: '/stats', component: ChartPage },
