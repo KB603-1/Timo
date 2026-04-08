f38058aa187c2f255541b9f8761b73165160cb88
catlover <jimini1183@naver.com>
2026-04-08 09:26:38 +0900

    feat: 하단 네비게이션 및 화면 UI 초기 구성 (스크립트 미포함)

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import MainPage from '@/page/MainPage.vue'
+import ChartPage from '@/page/ChartPage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    { path: '/', component: MainPage },
+    { path: '/stats', component: ChartPage },
   ],
 })
 
