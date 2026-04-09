0df59458ffcf6f1c0013b0cf517235c450ea7ed0
SongTaeKwon <taekwon0321@gmail.com>
2026-04-09 13:29:15 +0900

    feat: 글로벌 모달 추가

---
+++ src/components/GlobalModal.vue
@@ -0,0 +1,23 @@
+<script setup>
+import {useModalStore} from "@/stores/modal.js";
+import {Dialog, DialogContent} from "@/components/ui/dialog/index.js";
+
+const modal = useModalStore();
+</script>
+
+<template>
+  <Dialog :open="modal.isOpen" @update:open="modal.closeModal">
+    <DialogContent class="max-h-[90vh] overflow-y-auto overflow-x-hidden">
+      <component
+          v-if="modal.component"
+          :is="modal.component"
+          v-bind="modal.props"
+          @close="modal.closeModal"
+      />
+    </DialogContent>
+  </Dialog>
+</template>
+
+<style scoped>
+
+</style>
